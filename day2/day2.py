ec488c64977e85c84c679f3770d125cb94d880b7
monzo94 <dmonzonis@protonmail.com>
2019-12-02 19:58:23 +0100

    Add day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 def process_opcode(pos, codes):
     opcode = codes[pos]
     if opcode == 1 or opcode == 2:
@@ -23,15 +26,34 @@ def process_program(codes):
         current += 4
 
 
+def find_input_matching_output(output, codes):
+    orig = deepcopy(codes)
+    for noun in range(100):
+        for verb in range(100):
+            codes[1] = noun
+            codes[2] = verb
+            process_program(codes)
+            if codes[0] == output:
+                return noun, verb  # Match found
+            # Reset program state
+            codes = deepcopy(orig)
+
+
 def main():
     with open("input.txt") as f:
         codes = [int(x) for x in f.read().split(',')]
-    
+
+    orig = deepcopy(codes)
+
     # Part 1
     codes[1] = 12
     codes[2] = 2
     process_program(codes)
     print(codes[0])
+
+    # Part 2
+    noun, verb = find_input_matching_output(19690720, orig)
+    print(100 * noun + verb)
 
 
 if __name__ == "__main__":
