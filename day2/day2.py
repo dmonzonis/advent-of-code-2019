3f17f11b413863e3799b0c4127de34734f980fcb
monzo94 <dmonzonis@protonmail.com>
2019-12-02 19:57:51 +0100

    Add day 2 part 1

---
+++ day2/day2.py
@@ -0,0 +1,38 @@
+def process_opcode(pos, codes):
+    opcode = codes[pos]
+    if opcode == 1 or opcode == 2:
+        # Assuming positions are well behaved (within bounds)
+        val1 = codes[codes[pos + 1]]
+        val2 = codes[codes[pos + 2]]
+        result_pos = codes[pos + 3]
+        if opcode == 1:
+            codes[result_pos] = val1 + val2
+        if opcode == 2:
+            codes[result_pos] = val1 * val2
+        return True
+    if opcode == 99:
+        return False  # Terminate program
+
+
+def process_program(codes):
+    current = 0
+    while current < len(codes):
+        if not process_opcode(current, codes):
+            # opcode 99 reached, halt program
+            return
+        current += 4
+
+
+def main():
+    with open("input.txt") as f:
+        codes = [int(x) for x in f.read().split(',')]
+    
+    # Part 1
+    codes[1] = 12
+    codes[2] = 2
+    process_program(codes)
+    print(codes[0])
+
+
+if __name__ == "__main__":
+    main()
