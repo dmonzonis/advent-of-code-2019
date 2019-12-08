4c1c9cad4348678a8a506672386b6ada7d5f634d
monzo94 <dmonzonis@protonmail.com>
2019-12-08 19:52:31 +0100

    Add day 8 part 2

--- day8/day8.py
+++ day8/day8.py
@@ -17,6 +17,28 @@ def part1(matrix):
     return (matrix[layer, :, :] == 1).sum() * (matrix[layer, :, :] == 2).sum()
 
 
+def decode_image(matrix):
+    # There's probably a more elegant and efficient way to do this with numpy but fuck that
+    image = np.ndarray((matrix.shape[1], matrix.shape[2]), dtype=int)
+    image.fill(-1)
+    
+    for layer in range(matrix.shape[0]):
+        for y in range(matrix.shape[1]):
+            for x in range(matrix.shape[2]):
+                if image[y, x] == -1 and matrix[layer, y, x] != 2:
+                    image[y, x] = matrix[layer, y, x]
+    return image
+
+
+def print_image(image):
+    buffer = ""
+    for y in range(image.shape[0]):
+        for x in range(image.shape[1]):
+            buffer += "." if image[y, x] == 0 else "#"
+        print(buffer)
+        buffer = ""
+
+
 def main():
     with open('input.txt') as f:
         data = [int(x) for x in list(f.read().strip())]
@@ -24,6 +46,9 @@ def main():
     # Part 1
     matrix = fill_matrix(data)
     print(part1(matrix))
+
+    # Part 2
+    print_image(decode_image(matrix))
 
 
 if __name__ == "__main__":
