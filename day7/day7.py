e6fe4e6cff84ecdf55c18168f287a1362bd6b325
monzo94 <dmonzonis@protonmail.com>
2019-12-08 10:03:09 +0100

    Refactor Intcode program

--- day7/day7.py
+++ day7/day7.py
@@ -1,87 +1,114 @@
 from itertools import permutations
 
 
-def get_address_value(codes, n, mode=0):
-    # mode 0: position mode
-    # mode 1: immediate mode
-    return codes[codes[n]] if mode == 0 else codes[n]
+class Intcode:
+    """Modelizes an Intcode computer which can load and run an Intcode program."""
+    def __init__(self, program=None):
+        self.codes = self.orig_codes = None
+        self.input_stack = []
+        if program:
+            self.load_program(program)
+        self.outputs = []
+        self.instruction_ptr = 0
+        self.halted = False
+    
+    def load_program(self, program):
+        # Make a copy of the program
+        self.codes = program[:]
+        self.orig_codes = self.codes[:]
+    
+    def restart(self):
+        if self.orig_codes:
+            self.codes = self.orig_codes[:]
+        self.instruction_ptr = 0
+        self.halted = False
+        self.input_stack = []
+        self.outputs = []
 
+    def get_address_value(self, n, mode=0):
+        # mode 0: position mode
+        # mode 1: immediate mode
+        return self.codes[self.codes[n]] if mode == 0 else self.codes[n]
 
-def get_mode(params, param):
-    """Returns the mode for the nth param, where the param is in reading order (0 is the first param)"""
-    if param >= len(params):
-        return 0
-    return params[-param - 1]
+    def get_mode(self, params, param):
+        """Return the mode for the nth param, where the param is in reading order (0 is the first param)"""
+        if param >= len(params):
+            return 0
+        return params[-param - 1]
 
+    def _process_instruction(self):
+        """Process the next instruction, given by the position of the instruction pointer."""
+        instruction = str(self.codes[self.instruction_ptr])
+        opcode = int(instruction[-2:])
+        params = [int(x) for x in instruction[:-2]]
+        num_params = 1
 
-def process_opcode(pos, codes, inputs=None):
-    """Process the given opcode and return the new position of the instruction pointer after the call.
-    If a list of inputs is provided, the first element of the list will be used as input if opcode 3
-    is found, and then popped from the list."""
-    instruction = str(codes[pos])
-    opcode = int(instruction[-2:])  # opcode is always the last 2 digits
-    params = [int(x) for x in instruction[:-2]]
-    num_params = 1
-    output = None
+        if opcode == 1 or opcode == 2:
+            # Sum or multiply and store result into address
+            num_params = 3
+            val1 = self.get_address_value(self.instruction_ptr + 1, self.get_mode(params, 0))
+            val2 = self.get_address_value(self.instruction_ptr + 2, self.get_mode(params, 1))
+            result_pos = self.codes[self.instruction_ptr + 3]
+            if opcode == 1:
+                self.codes[result_pos] = val1 + val2
+            if opcode == 2:
+                self.codes[result_pos] = val1 * val2
+        elif opcode == 3:  # Get input and store into address
+            if self.input_stack:
+                inp = self.input_stack.pop(0)
+            else:
+                inp = int(input("Input a number: "))
+            self.codes[self.codes[self.instruction_ptr + 1]] = inp
+        elif opcode == 4:  # Output value
+            output = self.get_address_value(self.instruction_ptr + 1, self.get_mode(params, 0))
+            self.outputs.append(output)
+            # print(output)
+        elif opcode == 5 or opcode == 6:  # Jump if true (5) / false (6)
+            num_params = 2
+            val = self.get_address_value(self.instruction_ptr + 1, self.get_mode(params, 0))
+            if (val != 0 and opcode == 5) or (val == 0 and opcode == 6):
+                self.instruction_ptr = self.get_address_value(self.instruction_ptr + 2, self.get_mode(params, 1))
+                return
+        elif opcode == 7:  # Less than
+            num_params = 3
+            val1 = self.get_address_value(self.instruction_ptr + 1, self.get_mode(params, 0))
+            val2 = self.get_address_value(self.instruction_ptr + 2, self.get_mode(params, 1))
+            self.codes[self.codes[self.instruction_ptr + 3]] = int(val1 < val2)
+        elif opcode == 8:  # Equals
+            num_params = 3
+            val1 = self.get_address_value(self.instruction_ptr + 1, self.get_mode(params, 0))
+            val2 = self.get_address_value(self.instruction_ptr + 2, self.get_mode(params, 1))
+            self.codes[self.codes[self.instruction_ptr + 3]] = int(val1 == val2)
+        elif opcode == 99:
+            # Halt instruction; terminate program
+            self.halted = True
+            return
 
-    if opcode == 1 or opcode == 2:  # Sum or multiply and store result into address
-        num_params = 3
-        val1 = get_address_value(codes, pos + 1, get_mode(params, 0))
-        val2 = get_address_value(codes, pos + 2, get_mode(params, 1))
-        result_pos = codes[pos + 3]
-        if opcode == 1:
-            codes[result_pos] = val1 + val2
-        if opcode == 2:
-            codes[result_pos] = val1 * val2
-    elif opcode == 3:  # Get input and store into address
+        # Move instruction poniter by this opcode + number of params
+        self.instruction_ptr += num_params + 1
+    
+    def execute(self, inputs=None):
+        if not self.codes:
+            print("No program loaded!")
+            return
         if inputs:
-            inp = inputs.pop(0)
-        else:
-            inp = int(input("Input a number: "))
-        codes[codes[pos + 1]] = inp
-    elif opcode == 4:  # Output value
-        output = get_address_value(codes, pos + 1, get_mode(params, 0))
-        # print(output)
-    elif opcode == 5 or opcode == 6:  # Jump if true (5) / false (6)
-        num_params = 2
-        val = get_address_value(codes, pos + 1, get_mode(params, 0))
-        if (val != 0 and opcode == 5) or (val == 0 and opcode == 6):
-            return get_address_value(codes, pos + 2, get_mode(params, 1)), None
-    elif opcode == 7:  # Less than
-        num_params = 3
-        val1 = get_address_value(codes, pos + 1, get_mode(params, 0))
-        val2 = get_address_value(codes, pos + 2, get_mode(params, 1))
-        codes[codes[pos + 3]] = int(val1 < val2)
-    elif opcode == 8:  # Equals
-        num_params = 3
-        val1 = get_address_value(codes, pos + 1, get_mode(params, 0))
-        val2 = get_address_value(codes, pos + 2, get_mode(params, 1))
-        codes[codes[pos + 3]] = int(val1 == val2)
-    elif opcode == 99:
-        return None, None  # Terminate program
-
-    # Move instruction poniter by this opcode + number of params
-    # Also return output, if any
-    return pos + num_params + 1, output
-
-
-def run_program(codes, inputs):
-    current = 0
-    output = None
-    while current < len(codes):
-        current, aux = process_opcode(current, codes, inputs)
-        if aux is not None:
-            output = aux
-        if current is None:
-            # opcode 99 reached, halt program
-            return output  # Return final output
+            self.input_stack = inputs[:]
+        self.instruction_ptr = 0
+        while self.instruction_ptr < len(self.codes) and not self.halted:
+            self._process_instruction()
+    
+    def pop_output(self):
+        if self.outputs:
+            return self.outputs.pop(0)
 
 
 def run_amplifiers(codes, phases):
     output = 0
     # Run amps sequentially
     for phase in phases:
-        output = run_program(codes, [phase, output])
+        amp = Intcode(codes)
+        amp.execute([phase, output])
+        output = amp.pop_output()
     return output
 
 
@@ -98,8 +125,9 @@ def main():
     with open("input.txt") as f:
         data = [int(x) for x in f.read().strip().split(',')]
 
+    # Part 1
     print(find_highest_signal_combination(data))
-
+    
 
 if __name__ == "__main__":
     main()
